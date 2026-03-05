5c2bdbd84a71fd6ab86f8d12ad22218c84b2731c
Master <49209220+Master-s@users.noreply.github.com>
2026-03-05 20:11:00 +0400

    Add files via upload

---
+++ Vue Icons/bin/Debug/net8.0-windows/vue.bat
@@ -0,0 +1,3 @@
+@echo off
+vue\socat\socat.exe -t 99999999 - TCP:192.168.137.96:9090< vue\payloads\Img_54r.elf
+pause
---
+++ Vue Icons/bin/Debug/net8.0-windows/y2jb.bat
@@ -0,0 +1,3 @@
+@echo off
+vue\socat\socat.exe -t 99999999 - TCP:192.168.137.96:9090< vue\payloads\vue_icon_0.elf
+pause
